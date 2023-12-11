01a6dba6ab5042dc10d21a2ca7f484563b76ba2b
Doniel Acosta <donielacosta1995@gmail.com>
2023-12-10 20:26:40 -0400

    rutas

--- index.js
+++ index.js
@@ -1,61 +1,13 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) =>{
-  res.send("Hola mi server en Express");
+const express=require('express');
+const routerApi=require('./routes');
+const app=express();
+const port=3000;
+app.get('/',(req,res)=>{
+  res.send('Hola mi server en express');
 });
-
-app.get("/products", (req, res) =>{
-  res.json([
-    {
-      name: 'product 1',
-    price:400
-  },
-  {
-    name: 'product 2',
-    price:4000
-  },
-  ]);
+app.get('/nueva-ruta',(req,res)=>{
+  res.send('Hola, soy una nueva ruta');
 });
-
-app.get('/products/:id',(req,res)=>{
-  const {id} = req.params;
-  res.json({
-    id,
-    name: 'product 2',
-    price:4000
-  })
-})
-
-app.get('/categories/:categoryId/products/:productsId',(req,res)=>{
-  const {categoryId,productsId} = req.params;
-  res.json({
-    categoryId,
-    productsId,
-    name: 'product 2',
-    price:4000
-  })
-})
-
-app.listen(port, () =>{
-  console.log("My port: " + port);
+routerApi(app);app.listen(port,()=>{
+  console.log('Mi port'+port);
 });
-
-
-
-// const express = require('express');
-
-// const productsRouter = require('./products.router');
-// const categoriesRouter = require('./categories.router');
-// const usersRouter = require('./users.router');
-
-// function routerApi(app) {
-//   const router = express.Router();
-//   app.use('/api/v1', router);
-//   router.use('/products', productsRouter);
-//   router.use('/categories', categoriesRouter);
-//   router.use('/users', usersRouter);
-// }
-
-// module.exports = routerApi;
