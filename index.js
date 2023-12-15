60f8fa96cebc0667f3d904191924534386f57d09
doniel <donielacosta1995@gmail.com>
2023-12-15 12:00:25 -0400

    cors

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express=require('express');
+const cors =require('cors');
 const routerApi=require('./routes');
+const {logErrors,errorHandler,boomErrorHandler} =require('./middleware/error.handler');
 const app=express();
 const port=3000;
 app.get('/',(req,res)=>{
   res.send('Hola mi server en express');
 });
 app.use(express.json());
+app.use(cors());
 app.get('/nueva-ruta',(req,res)=>{
   res.send('Hola, soy una nueva ruta');
 });
-routerApi(app);app.listen(port,()=>{
-  console.log('Mi port'+port);
+routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port,()=>{
+  console.log('Mi port' + port);
 });
